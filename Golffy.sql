07517858778e3fc5f53b07099957ffe74fcb2bf7
ToYouMyLight <110706371+ToYouMyLight@users.noreply.github.com>
2022-11-15 01:09:36 +0900

    Update Golffy.sql
    DB Design

--- Golffy.sql
+++ Golffy.sql
@@ -18,23 +18,38 @@ board_title varchar(50) not null,
 board_content varchar(200) not null,
 board_viewcnt int default 0,
 board_like int default 0,
+deleted boolean default false,
 foreign key(user_pid) references user(user_pid)
 )ENGINE=InnoDB;
 
-create table if not exists comment (
+create table if not exists cmt (
 comment_id int auto_increment primary key,
 board_id int not null,
 user_pid int not null,
-comment varchar(100) not null,
+cmt varchar(100) not null,
+deleted boolean default false,
 foreign key(board_id) references board(board_id),
 foreign key(user_pid) references user(user_pid)
 )ENGINE=InnoDB;
 
-create table if not exists user_board (
-user_pid int primary key,
+create table if not exists subcmt (
+subcmt_id int auto_increment primary key,
+comment_id int not null,
+user_pid int not null,
+cmt varchar(100) not null,
+deleted boolean default false,
+foreign key(comment_id) references cmt(comment_id),
+foreign key(user_pid) references user(user_pid)
+)ENGINE=InnoDB;
+
+create table if not exists files (
+file_id int auto_increment primary key,
 board_id int not null,
-foreign key(user_pid) references user(user_pid),
-foreign key(board_id) references board(board_id)
+save_folder varchar(50) not null,
+origin_file varchar(50) not null,
+save_file varchar(50) not null,
+foreign key(board_id) references board(board_id),
+foreign key(user_pid) references user(user_pid)
 )ENGINE=InnoDB;
 
 create table if not exists user_like (
@@ -42,13 +57,6 @@ user_pid int primary key,
 board_id int not null,
 foreign key(user_pid) references user(user_pid),
 foreign key(board_id) references board(board_id)
-)ENGINE=InnoDB;
-
-create table if not exists user_comment (
-user_pid int primary key,
-comment_id int not null,
-foreign key(user_pid) references user(user_pid),
-foreign key(comment_id) references comment(comment_id)
 )ENGINE=InnoDB;
 
 create table if not exists follow (
